2aa7702cbb664a6433d9aa3a39442c71465ed467
Jim <Jimpotter88@gmail.com>
2025-01-25 20:21:25 +0200

    commit

--- main.js
+++ main.js
@@ -51,7 +51,6 @@ document.addEventListener('DOMContentLoaded', () => {
     camera.add(listener1);
     const audio1 = new THREE.PositionalAudio(listener1);
     raccoonAnchor.group.add(audio1);
-	audio1.setVolume(40);
     audio1.setBuffer(audioClip1);
     audio1.setRefDistance(100);
     audio1.setLoop(true);
@@ -73,10 +72,9 @@ document.addEventListener('DOMContentLoaded', () => {
     camera.add(listener2);
     const audio2 = new THREE.PositionalAudio(listener2);
     bearAnchor.group.add(audio2);
-	audio2.setVolume(40);
     audio2.setBuffer(audioClip2);
     audio2.setRefDistance(100);
-    audio2.setLoop(true);
+    audio2.setLoop(true); 
 
     bearAnchor.onTargetFound = () => {
       audio2.play();
